3e56e09d4b7929a2ab8d4e24bba2d5ab0d921c60
Daniel Reimer <reimer.daniel@freenet.de>
2011-07-25 22:51:16 +0000

    Update RosBE Options to the new RosBE Version 2.0.
    svn path=/trunk/RosBE/; revision=1307

--- RosBE-Windows/Tools/config/x86/options.c
+++ RosBE-Windows/Tools/config/x86/options.c
@@ -62,9 +62,9 @@ getConfigFileCMD()
         }
         else
         {
-            if ((wcslen(filename) + wcslen(L"\\RosBE\\rosbe-options-1.6.cmd")) < MAX_PATH)
+            if ((wcslen(filename) + wcslen(L"\\RosBE\\rosbe-options-2.0.cmd")) < MAX_PATH)
             {
-                wcscat(filename, L"\\RosBE\\rosbe-options-1.6.cmd");
+                wcscat(filename, L"\\RosBE\\rosbe-options-2.0.cmd");
             }
         }
     }
@@ -80,7 +80,7 @@ getConfigFileCMD()
         }
         else
         {
-            wcscpy(filename, L"rosbe-options-1.6.cmd");
+            wcscpy(filename, L"rosbe-options-2.0.cmd");
         }
     }
     return filename;
@@ -110,9 +110,9 @@ getConfigFilePS1()
         }
         else
         {
-            if ((wcslen(filename) + wcslen(L"\\RosBE\\rosbe-options-1.6.ps1")) < MAX_PATH)
+            if ((wcslen(filename) + wcslen(L"\\RosBE\\rosbe-options-2.0.ps1")) < MAX_PATH)
             {
-                wcscat(filename, L"\\RosBE\\rosbe-options-1.6.ps1");
+                wcscat(filename, L"\\RosBE\\rosbe-options-2.0.ps1");
             }
         }
     }
@@ -128,7 +128,7 @@ getConfigFilePS1()
         }
         else
         {
-            wcscpy(filename, L"rosbe-options-1.6.ps1");
+            wcscpy(filename, L"rosbe-options-2.0.ps1");
         }
     }
     return filename;
