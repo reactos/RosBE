54cdf02e1924114e74e1830449281e63dcce3748
Colin Finck <colin@reactos.org>
2019-08-18 15:20:21 +0200

    [BUILDTOOLCHAIN] Sync setuplibrary.sh changes from RosBE-Unix and add required MSYS conditionals.

--- RosBE-Windows/Buildtoolchain/scripts/setuplibrary.sh
+++ RosBE-Windows/Buildtoolchain/scripts/setuplibrary.sh
@@ -17,12 +17,26 @@
 #   Parameter 2: Optional additional parameters for "echo"
 rs_boldmsg()
 {
-	echo -e $2 "\e[1m$1\e[0m"
+	echo -e $2 "\033[1m$1\033[0m"
 }
 
 # Check for several requirements, which need to be met in order to run the installation script properly
 rs_check_requirements()
 {
+	# Check for the processor architecture
+	local cpuarch=`uname -m`
+	case "$cpuarch" in
+		"i686")
+			rs_abi=32
+			;;
+		"x86_64" | "amd64")
+			rs_abi=64
+			;;
+		*)
+			echo "Your processor architecture is not supported by RosBE-Unix!"
+			exit 1;;
+	esac
+
 	# Test if the script directory is writable
 	if [ ! -w "$rs_scriptdir" ]; then
 		rs_redmsg "The script directory \"$rs_scriptdir\" is not writable, aborted!"
@@ -62,7 +76,7 @@ rs_check_requirements()
 	local app
 	local checkapps="make gmake"
 	rs_makecmd=""
-	
+
 	for app in $checkapps; do
 		if $app -v 2>&1 | grep "GNU Make" >& /dev/null; then
 			# Store the complete path in $rs_makecmd to prevent collisions with our own Make.
@@ -70,13 +84,41 @@ rs_check_requirements()
 			rs_greenmsg "OK"
 		fi
 	done
-	
+
 	if [ "$rs_makecmd" = "" ]; then
 		rs_redmsg "MISSING"
 		toolmissing=true
 	fi
 
-	
+	# Check for libs
+	# Skip that part on OSX and MSYS
+	if [ "`uname`" != "Darwin" ] && [ "`uname -o`" != "Msys" ]; then
+		# pkg-config needs to be installed to check for libs
+		echo -n "Checking for pkg-config... "
+
+		if which pkg-config >& /dev/null; then
+			rs_greenmsg "OK"
+		else
+			rs_redmsg "MISSING"
+			toolmissing=true
+		fi
+
+		for lib in $rs_needed_libs; do
+			echo -n "Checking for $lib... "
+
+			if pkg-config --exists $lib >& /dev/null; then
+				rs_greenmsg "OK"
+			else
+				if which pkg-config >& /dev/null; then
+					rs_redmsg "MISSING"
+				else
+					rs_redmsg "UNABLE TO DETERMINE"
+				fi
+				toolmissing=true
+			fi
+		done
+	fi
+
 	if $toolmissing; then
 		echo "At least one needed tool is missing, aborted!"
 		exit 1
@@ -88,15 +130,22 @@ rs_check_requirements()
 # Check whether the previous command finished with errorlevel 0
 # If it did so, print a green "OK" and delete the debug logfile for the command.
 # If that wasn't the case, print a red "FAILED" and give information about the debug logfile.
+#   Parameter 1: Can fail, if set to 0, failure leads to process termination
 rs_check_run()
 {
 	if [ $? -ne 0 ]; then
-		rs_redmsg "FAILED"
-		echo "Please take a look at the log file \"$rs_workdir/build.log\""
-		echo "If you did not do something wrong, please save the log file and contact the ReactOS Team."
+		if [ $1 -eq 0 ]; then
+			rs_redmsg "FAILED"
+			echo "Please take a look at the log file \"$rs_workdir/build.log\""
+			echo "If you did not do something wrong, please save the log file and contact the ReactOS Team."
 
-		echo "Aborted!"
-		exit 1
+			echo "Aborted!"
+			exit 1
+		else
+			rs_yellowmsg "ERRORS"
+			cat "$rs_workdir/build.log" >> "$rs_workdir/build-ignored.log"
+			rm "$rs_workdir/build.log"
+		fi
 	else
 		rs_greenmsg "OK"
 		rm "$rs_workdir/build.log"
@@ -120,7 +169,17 @@ rs_do_command()
 {
 	echo -n "Running \"$*\"... "
 	$* >& "$rs_workdir/build.log"
-	rs_check_run
+	rs_check_run 0
+}
+
+# Executes a building command and checks whether it succeeded.
+# Doesn't terminate the building process in case of failure.
+#   Parameters: The command to execute including parameters
+rs_do_command_can_fail()
+{
+	echo -n "Running \"$*\"... "
+	$* >& "$rs_workdir/build.log"
+	rs_check_run 1
 }
 
 # Checks whether the given module needs to be processed and if so, extracts it.
@@ -131,7 +190,7 @@ rs_extract_module()
 {
 	local module=$1
 	local target_dir=$2
-	
+
 	if ! `eval echo \\$rs_process_$module`; then
 		return 1
 	fi
@@ -142,7 +201,7 @@ rs_extract_module()
 
 	# Extract with bunzip2 and tar instead of "tar xjf" due to https://github.com/msys2/MSYS2-packages/issues/1548
 	bunzip2 --decompress --stdout "$rs_sourcedir/$module.tar.bz2" | tar -x --file=- >& "$rs_workdir/build.log"
-	rs_check_run
+	rs_check_run 0
 
 	return 0
 }
@@ -152,7 +211,15 @@ rs_extract_module()
 #   Parameter 2: Optional additional parameters for "echo"
 rs_greenmsg()
 {
-	echo -e $2 "\e[32m$1\e[0m"
+	echo -e $2 "\033[32m$1\033[0m"
+}
+
+# Print a message in yellow color
+#   Parameter 1: The message
+#   Parameter 2: Optional additional parameters for "echo"
+rs_yellowmsg()
+{
+	echo -e $2 "\033[33m$1\033[0m"
 }
 
 # Creates the given directory if it does not exist and cleans it if it does.
@@ -162,7 +229,7 @@ rs_mkdir_empty()
 	if [ -d "$1" ]; then
 		rm -rf "$1"
 	fi
-	
+
 	mkdir -p "$1"
 }
 
@@ -191,7 +258,7 @@ rs_prepare_module()
 	rm -rf "$module-build"
 	mkdir "$module-build"
 	cd "$module-build"
-	
+
 	return 0
 }
 
@@ -200,7 +267,7 @@ rs_prepare_module()
 #   Parameter 2: Optional additional parameters for "echo"
 rs_redmsg()
 {
-	echo -e $2 "\e[31m$1\e[0m"
+	echo -e $2 "\033[31m$1\033[0m"
 }
 
 # Allow a choice between several options
