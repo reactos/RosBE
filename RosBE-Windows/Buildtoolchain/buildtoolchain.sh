7edb44b5214e7d5ea2118e6f7d8ac75f932f9cde
Colin Finck <colin@reactos.org>
2019-08-19 09:12:09 +0200

    [BUILDTOOLCHAIN] Fix host/build/target triplets for the Windows toolchain, use NTFS links in MSYS (requires Win10 or elevated MSYS prompt for other NT6+ versions), and suggest autorebase.bat

--- RosBE-Windows/Buildtoolchain/buildtoolchain.sh
+++ RosBE-Windows/Buildtoolchain/buildtoolchain.sh
@@ -34,6 +34,9 @@ CC=gcc
 rs_makecmd=make
 use_cflags=0
 
+# Make MSYS use native NTFS links for "ln -s"
+export MSYS=winsymlinks:nativestrict
+
 # RosBE Setup Variables
 rs_host_cflags="-pipe -O2 -Wl,-S -g0 -march=core2 -mfpmath=sse -msse2"
 rs_needed_tools="as bison bzip2 find flex $CC $CXX grep makeinfo python re2c tar"        # GNU Make has a special check
@@ -91,7 +94,17 @@ fi
 
 # Install required tools in MSYS2
 rs_boldmsg "Running MSYS pacman..."
-pacman -S --quiet --noconfirm --needed bison diffutils flex make python re2c texinfo
+pacman -S --quiet --noconfirm --needed bison diffutils flex make python re2c texinfo | tee /tmp/buildtoolchain-pacman.log
+
+if grep installation /tmp/buildtoolchain-pacman.log >& /dev/null; then
+	# See e.g. https://sourceforge.net/p/msys2/tickets/74/
+	echo
+	rs_boldmsg "Installed MSYS packages have changed!"
+	echo "For a successful toolchain build, this requires you to close all MSYS windows and run \"autorebase.bat\" in the MSYS installation directory."
+	echo "After you have done so, please rerun \"buildtoolchain.sh\"."
+	exit 1
+fi
+
 echo
 
 rs_check_requirements
@@ -167,7 +180,7 @@ if rs_prepare_module "binutils"; then
 		export CFLAGS="$rs_host_cflags"
 	fi
 
-	rs_do_command ../binutils/configure --prefix="$rs_archprefixdir" --with-sysroot="$rs_archprefixdir" --target="$rs_target" --disable-multilib --disable-werror --enable-lto --enable-plugins --with-zlib=yes --disable-nls
+	rs_do_command ../binutils/configure --prefix="$rs_archprefixdir" --host="$rs_target" --build="$rs_target" --target="$rs_target" --with-sysroot="$rs_archprefixdir" --disable-multilib --disable-werror --enable-lto --enable-plugins --with-zlib=yes --disable-nls
 	rs_do_command $rs_makecmd -j $rs_cpucount
 	rs_do_command $rs_makecmd install
 	rs_clean_module "binutils"
@@ -210,7 +223,7 @@ if rs_prepare_module "gcc"; then
 	export CFLAGS_FOR_TARGET="$rs_target_cflags"
 	export CXXFLAGS_FOR_TARGET="$rs_target_cflags"
 
-	rs_do_command ../gcc/configure --target="$rs_target" --prefix="$rs_archprefixdir" --with-sysroot="$rs_archprefixdir" --with-pkgversion="RosBE-Windows" --enable-languages=c,c++ --enable-fully-dynamic-string --enable-checking=release --enable-version-specific-runtime-libs --enable-plugins --disable-shared --disable-multilib --disable-nls --disable-werror --disable-win32-registry --disable-sjlj-exceptions --disable-libstdcxx-verbose
+	rs_do_command ../gcc/configure --prefix="$rs_archprefixdir" --host="$rs_target" --build="$rs_target" --target="$rs_target" --with-sysroot="$rs_archprefixdir" --with-pkgversion="RosBE-Windows" --enable-languages=c,c++ --enable-fully-dynamic-string --enable-checking=release --enable-version-specific-runtime-libs --enable-plugins --disable-shared --disable-multilib --disable-nls --disable-werror --disable-win32-registry --disable-sjlj-exceptions --disable-libstdcxx-verbose
 	rs_do_command $rs_makecmd -j $rs_cpucount all-gcc
 	rs_do_command $rs_makecmd install-gcc
 	rs_do_command_can_fail $rs_makecmd install-lto-plugin
@@ -220,7 +233,7 @@ if rs_prepare_module "gcc"; then
 			export CFLAGS="$rs_host_cflags"
 		fi
 
-		rs_do_command ../mingw_w64/mingw-w64-crt/configure --prefix="$rs_archprefixdir/$rs_target" --with-sysroot="$rs_archprefixdir/$rs_target" --host="$rs_target"
+		rs_do_command ../mingw_w64/mingw-w64-crt/configure --prefix="$rs_archprefixdir/$rs_target" --host="$rs_target" --with-sysroot="$rs_archprefixdir/$rs_target"
 		rs_do_command $rs_makecmd -j $rs_cpucount
 		rs_do_command $rs_makecmd install
 		rs_clean_module "mingw_w64"
